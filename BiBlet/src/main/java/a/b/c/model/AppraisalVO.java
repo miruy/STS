d02ef7d26d28bb5d4e4654c16a99432c7ce75283
miruy <dbflarla4966@naver.com>
2022-04-06 17:42:28 +0900

    평가 작성 기능 수정

--- BiBlet/src/main/java/a/b/c/model/AppraisalVO.java
+++ BiBlet/src/main/java/a/b/c/model/AppraisalVO.java
@@ -1,21 +1,19 @@
 package a.b.c.model;
 
-import java.util.Date;
-
 public class AppraisalVO {
 			
 			private Long mem_num;	//회원 번호(PK,FK)
 			private String isbn;	//도서 일련번호(PK,FK)
 			private String star;	//별점
 			private String book_comment;	//코멘트(평가)
-			private Date start_date;	//도서 구독 시작 날짜
-			private Date end_date;	//도서 구독 완료 날짜
+			private String start_date;	//도서 구독 시작 날짜
+			private String end_date;	//도서 구독 완료 날짜
 			private String co_prv;	//코멘트 공개 여부
 			 
 			public AppraisalVO(){}
 			
 			public AppraisalVO(Long mem_num, String isbn, String star, String book_comment, 
-												Date start_dater, Date end_date, String co_prv) {
+					String start_dater, String end_date, String co_prv) {
 				this.mem_num = mem_num;
 				this.isbn = isbn;
 				this.star = star;
@@ -57,19 +55,19 @@ public class AppraisalVO {
 				this.book_comment = book_comment;
 			}
 
-			public Date getStart_date() {
+			public String getStart_date() {
 				return start_date;
 			}
 
-			public void setStart_date(Date start_date) {
+			public void setStart_date(String start_date) {
 				this.start_date = start_date;
 			}
 
-			public Date getEnd_date() {
+			public String getEnd_date() {
 				return end_date;
 			}
 
-			public void setEnd_date(Date end_date) {
+			public void setEnd_date(String end_date) {
 				this.end_date = end_date;
 			}
 
