56eee9fdf0020c321f8e88d2edbf50fe59cb47b2
miruy <dbflarla4966@naver.com>
2022-04-08 00:18:49 +0900

    DB table및 VO수정

--- BiBlet/src/main/java/a/b/c/model/AppraisalVO.java
+++ BiBlet/src/main/java/a/b/c/model/AppraisalVO.java
@@ -1,83 +1,93 @@
 package a.b.c.model;
 
 public class AppraisalVO {
-			
-			private Long mem_num;	//회원 번호(PK,FK)
-			private Long appraisal_num;	//평가 번호(PK)
-			private String star;	//별점
-			private String book_comment;	//코멘트(평가)
-			private String start_date;	//도서 구독 시작 날짜
-			private String end_date;	//도서 구독 완료 날짜
-			private String co_prv;	//코멘트 공개 여부
-			 
-			public AppraisalVO(){}
-			
-			public AppraisalVO(Long mem_num, Long appraisal_num, String isbn, String star, String book_comment, 
-					String start_dater, String end_date, String co_prv) {
-				this.mem_num = mem_num;
-				this.appraisal_num = appraisal_num;
-				this.star = star;
-				this.book_comment = book_comment;
-				this.start_date = start_dater;
-				this.end_date = end_date;
-				this.co_prv = co_prv;
-			}
 
-			public Long getMem_num() {
-				return mem_num;
-			}
+	private Long appraisal_num; // 평가 번호(시퀀스)
+	private String star; // 별점
+	private String book_comment; // 코멘트(평가)
+	private String start_date; // 도서 구독 시작 날짜
+	private String end_date; // 도서 구독 완료 날짜
+	private String co_prv; // 코멘트 공개 여부
+	private Long mem_num; // 회원 번호(FK)
+	private Long book_num; // 도서 번호(FK)
 
-			public void setMem_num(Long mem_num) {
-				this.mem_num = mem_num;
-			}
+	public AppraisalVO() {}
 
-			public Long getAppraisal_num() {
-				return appraisal_num;
-			}
+	public AppraisalVO(Long appraisal_num, String isbn, String star, String book_comment, String start_dater,
+			String end_date, String co_prv, Long mem_num, Long book_num) {
 
-			public void setAppraisal_num(Long appraisal_num) {
-				this.appraisal_num = appraisal_num;
-			}
+		this.appraisal_num = appraisal_num;
+		this.star = star;
+		this.book_comment = book_comment;
+		this.start_date = start_dater;
+		this.end_date = end_date;
+		this.co_prv = co_prv;
+		this.mem_num = mem_num;
+		this.book_num = book_num;
+	}
 
-			public String getStar() {
-				return star;
-			}
+	public Long getAppraisal_num() {
+		return appraisal_num;
+	}
 
-			public void setStar(String star) {
-				this.star = star;
-			}
+	public void setAppraisal_num(Long appraisal_num) {
+		this.appraisal_num = appraisal_num;
+	}
 
-			public String getBook_comment() {
-				return book_comment;
-			}
+	public String getStar() {
+		return star;
+	}
 
-			public void setBook_comment(String book_comment) {
-				this.book_comment = book_comment;
-			}
+	public void setStar(String star) {
+		this.star = star;
+	}
 
-			public String getStart_date() {
-				return start_date;
-			}
+	public String getBook_comment() {
+		return book_comment;
+	}
 
-			public void setStart_date(String start_date) {
-				this.start_date = start_date;
-			}
+	public void setBook_comment(String book_comment) {
+		this.book_comment = book_comment;
+	}
 
-			public String getEnd_date() {
-				return end_date;
-			}
+	public String getStart_date() {
+		return start_date;
+	}
 
-			public void setEnd_date(String end_date) {
-				this.end_date = end_date;
-			}
+	public void setStart_date(String start_date) {
+		this.start_date = start_date;
+	}
 
-			public String getCo_prv() {
-				return co_prv;
-			}
+	public String getEnd_date() {
+		return end_date;
+	}
 
-			public void setCo_prv(String co_prv) {
-				this.co_prv = co_prv;
-			}
+	public void setEnd_date(String end_date) {
+		this.end_date = end_date;
+	}
 
-			
+	public String getCo_prv() {
+		return co_prv;
+	}
+
+	public void setCo_prv(String co_prv) {
+		this.co_prv = co_prv;
+	}
+
+	public Long getMem_num() {
+		return mem_num;
+	}
+
+	public void setMem_num(Long mem_num) {
+		this.mem_num = mem_num;
+	}
+
+	public Long getBook_num() {
+		return book_num;
+	}
+
+	public void setBook_num(Long book_num) {
+		this.book_num = book_num;
+	}
+
 }
